639ca39dbd1594f5b4bbbccd6c7c927b8b6d8683
iKirill <k.ivashchenko@gmail.com>
2020-06-10 23:05:09 -0400

    Sync

--- Sandbox/src/tech/kitcorp/Main.java
+++ Sandbox/src/tech/kitcorp/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	double firstNum = 2;
 	double secondNum = 2;
-//        System.out.println(firstNum^2);
         System.out.println(Math.pow(firstNum, secondNum));
     }
 }

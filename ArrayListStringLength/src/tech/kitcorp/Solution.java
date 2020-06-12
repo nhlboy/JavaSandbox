a093bcc608f0d67082e45874333dbbed6b8d2cc0
nhlboy <k.ivashchenko@gmail.com>
2020-06-12 03:16:34 -0400

    Upload

--- ArrayListStringLength/src/tech/kitcorp/Solution.java
+++ ArrayListStringLength/src/tech/kitcorp/Solution.java
@@ -11,29 +11,38 @@ import java.util.ArrayList;
 public class Solution {
     private static ArrayList<String> strings;
 
+    private static ArrayList<Integer> stringSizes = new ArrayList<Integer>();
+
     public static void main(String[] args) throws Exception {
+
+        strings = new ArrayList<String>();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        ArrayList<String> listStrings = new ArrayList<String>();
-        for (int i = 0; i < 5; i++)
-        {
+        // заполняем ArrayList
+        for (int i = 0; i < 5; i++) {
             String s = reader.readLine();
-            listStrings.add(s);
+            strings.add(s);
         }
 
-        ArrayList<Integer> stringsLenght = new ArrayList<Integer>();
-        for (String item : strings) {
-            stringsLenght.add(item.length());//add length to list
+        int max = strings.get(0).length();
+
+        for (int i = 0; i < 5; i++) {
+            if (max < strings.get(i).length()) {
+                max = strings.get(i).length();
+            }
         }
-        System.out.println(stringsLenght);
 
-
+        for (int i = 0; i < 5; i++) {
+            if (max == strings.get(i).length()) System.out.println(strings.get(i));
+        }
     }
 }
 
+
 // 1. Создай список строк.
 // 2. Считай с клавиатуры 5 строк и добавь в список.
+
 // 3. Используя цикл, найди самую длинную строку в списке.
 // 4. Выведи найденную строку на экран. Если таких строк несколько, выведи каждую с новой строки.
 
@@ -42,3 +51,9 @@ public class Solution {
 // •	Программа должна считывать 5 строк с клавиатуры и записывать их в поле strings класса Solution.
 // •	Программа должна выводить самую длинную строку на экран.
 // •	Если есть несколько строк с длиной равной максимальной, то нужно вывести каждую из них с новой строки.
+
+//    дедушка
+//    бабушка
+//    дочь
+//    программа
+//    машина
